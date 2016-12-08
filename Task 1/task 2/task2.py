abc787082214b520bf17b2fd4ff922a2daa05244
Rohit Raj <rohit314.raj@gmail.com>
2016-12-08 01:45:47 -0500

    bm25-pseudo relevance

--- Task 1/task 2/task2.py
+++ Task 1/task 2/task2.py
@@ -148,87 +148,15 @@ def bm25_score_calculation(avdl):
 
 
 def sort_score_map(score_map):
-    sorted_score_map = {}
     for i in range (1,len(query_map)+1):
         sorted_score_map[str(i)] = {}
-        
         sorted_docs = sorted(score_map[str(i)].items(), key=operator.itemgetter(1),reverse = True)
         rank = 1
         for doc_score_tuple in sorted_docs:
             sorted_score_map[str(i)][rank] = doc_score_tuple[0]
             rank += 1
-        #print("max rank: "+ str(i) +" "+str(rank))
     return sorted_score_map
-
-def relevant_doc_terms(sorted_score_map,R):
-    terms_in_rel_list = {}
-    for query_no in query_map:
-        terms_in_rel_list[query_no] = []
-        for rank in range(1,R+1):
-            doc_rel = sorted_score_map[query_no][rank]
-            for term in doc_term_score[doc_rel]:
-                #print(term)
-                terms_in_rel_list[query_no].append(term)
-    return terms_in_rel_list
         
-    
-
-def calculate_new_query_scores(sorted_score_map,R):
-    alpha = 8.0
-    beta = 16.0
-    gamma = 4.0
-    terms_in_rel_list = relevant_doc_terms(sorted_score_map,R)
-    for q in range(1,len(query_map)+1):
-        query_no = str(q)
-        print(query_no)
-        temp_query_vector = {}
-        temp_query_vector[query_no] = {}
-        for term in terms_in_rel_list[query_no]:
-            in_query_component = alpha*float(query_map[query_no].count(term))
-            summation_dij = 0.0
-            for rank in range(1,R+1):
-                doc_rel = sorted_score_map[query_no][rank]
-                if term in doc_term_score[doc_rel]:
-                    dij = doc_term_score[doc_rel][term]
-                    summation_dij += dij
-                
-                rank += 1
-            rel_doc_component = beta*(1/R)*summation_dij
-
-            summation_dij = 0.0
-            
-            for rank in range(R+1,len(sorted_score_map[query_no])+1):
-                doc_nonrel = sorted_score_map[query_no][rank]
-                if term in doc_term_score[doc_nonrel]:
-                    dij = doc_term_score[doc_nonrel][term]
-                    summation_dij += dij
-                rank += 1
-
-            nonrel_docs = len(non_inverted_index) - R
-            nonrel_doc_component = gamma*(1/nonrel_docs)*summation_dij
-
-            new_query_score = in_query_component + rel_doc_component - nonrel_doc_component
-
-            
-            
-            if(new_query_score > 0.0):
-                temp_query_vector[query_no][term] = new_query_score
-                #print(temp_query_vector)
-
-        #print(temp_query_vector)
-        sorted_term_scores = sorted(temp_query_vector[query_no].items(), key=operator.itemgetter(1),reverse = True)
-        at = 0
-        
-        for term_score in sorted_term_scores:
-            #print(str(at)+" "+term_score[0]+" "+str(term_score[1]))
-            query_map[query_no].append(term_score[0])
-            at += 1
-            if at == 20:
-                break
-        
-        
-            
-    
 
 def write_results_to_file(score_map):
     file_h = open("cacm_query_bm25.txt","w")
@@ -244,9 +172,9 @@ def write_results_to_file(score_map):
             if rank > 100:
                 break
     file_h.close()
-    
-def write_results_to_file2(score_map):
-    file_h = open("cacm_query_bm25_af.txt","w")
+
+'''def write_results_to_file(score_map):
+    file_h = open("cacm_query_bm25.txt","w")
     for i in range (1,len(query_map)+1):
         sorted_docs = sorted(score_map[str(i)].items(), key=operator.itemgetter(1),reverse = True)
 
@@ -258,36 +186,24 @@ def write_results_to_file2(score_map):
 
             if rank > 100:
                 break
-    file_h.close()
+    file_h.close()'''
+    
+
 
 def bm25():
     load_inverted_index()
+
     calculate_dij()
-    #print(doc_term_score['0001'])
+    print(doc_term_score['0001'])
     
     load_queries()
-    '''for q in range(1,len(query_map)+1):
-        query_no = str(q)
-        print(query_no+" "+str(len(query_map[query_no])))'''
-        
+
     avdl = calculate_avdl()
     
     score_map = bm25_score_calculation(avdl)
     sorted_score_map = sort_score_map(score_map)
+    print(sorted_score_map['1'])
     write_results_to_file(score_map)
-    #print(sorted_score_map['1'])
-
-    #pseudo relevance - docs above rank R are assumed to be relevant
-    R = 50
-    calculate_new_query_scores(sorted_score_map,R)
-    '''for q in range(1,len(query_map)+1):
-        query_no = str(q)
-        print(query_no+" "+str(len(query_map[query_no])))'''
-
-    score_map = bm25_score_calculation(avdl)
-    sorted_score_map = sort_score_map(score_map)
-    write_results_to_file2(score_map)
-    
 
 
 bm25()
